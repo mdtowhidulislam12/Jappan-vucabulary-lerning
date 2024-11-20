634b4f0673402e4f447de639a9ee0855d9d9403a
unknown <mohammadtowhidulislam999@gmail.com>
2024-11-20 20:40:27 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBmDHJfBa33_5CtKwTJBD7a2-gna4_2nWc",
+  authDomain: "vocabulary-learning-a9.firebaseapp.com",
+  projectId: "vocabulary-learning-a9",
+  storageBucket: "vocabulary-learning-a9.firebasestorage.app",
+  messagingSenderId: "1092215035055",
+  appId: "1:1092215035055:web:f3a7e3b7e1933f7cadf05a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig); 
