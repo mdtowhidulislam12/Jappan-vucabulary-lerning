584f843f15d19c14208993e9c66ed7cb4c3da584
unknown <mohammadtowhidulislam999@gmail.com>
2024-11-21 08:28:09 +0600

    header and footer is added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,46 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './LayOut/Layout';
+import Home from './pages/Home';
+import Tutorials from './pages/Tutorials';
+import Learning from './pages/Learning';
+import Aboutus from './pages/Aboutus';
+import Contact from './pages/Contact';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    children:[
+      {
+        path:'/',
+        element: <Home></Home>
+      },
+      {
+        path:'/start-learning',
+        element: <Learning></Learning>
+      },
+      {
+        path:'/tutorials',
+        element: <Tutorials></Tutorials>
+      },
+      {
+        path:'/about-us',
+        element: <Aboutus></Aboutus>
+      },
+      {
+        path:'/contact',
+        element: <Contact></Contact>
+      },
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
---
+++ src/pages/Tutorials.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Tutorials = () => {
+    return (
+        <div>
+            Tutorials page
+        </div>
+    );
+};
+
+export default Tutorials;
